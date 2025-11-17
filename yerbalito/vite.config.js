f182585ff1ca1796b975c68901ec6ef81f1b24f3
michaelrodriguezuy <hola@olimarteam.uy>
2025-11-16 23:40:07 -0300

    fix: correccion del uso de date-fns

--- yerbalito/vite.config.js
+++ yerbalito/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['date-fns'],
+    esbuildOptions: {
+      resolveExtensions: ['.js', '.jsx', '.ts', '.tsx', '.mjs'],
+    },
+  },
+  resolve: {
+    dedupe: ['date-fns'],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 })
